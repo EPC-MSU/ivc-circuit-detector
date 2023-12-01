d2818d749259f732a9661eff2bf73203be4ad3cf
Andrey Marakulin <annndruha.github@gmail.com>
2023-12-01 19:14:44 +0300

    Test raw PySpice

---
+++ generate_dataset/cir_parser.py
@@ -0,0 +1,16 @@
+import logging
+
+from PySpice.Logging import Logging
+from PySpice.Spice.Parser import SpiceParser
+
+Logging.setup_logging(logging_level=logging.ERROR)
+
+
+parser = SpiceParser(path='C:\dev\ivc-circuit-detector\circuit_classes\D_R\D_R.cir')
+circuit = parser.build_circuit()
+
+el = circuit['R1']
+circuit.R1.resistance = '2K'
+
+print(circuit)
+print(circuit['R1'])
